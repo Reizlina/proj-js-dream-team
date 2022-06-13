4068294ce9fe2b33904293d79cbc3344291c5c18
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-06-13 11:23:53 +0300

    suf

--- src/js/fetch-film.js
+++ src/js/fetch-film.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { UnsplashApi } from './themoviedb';
+import { createPagination } from './pagination';
 
-console.log();
+const paginationBtn = document.querySelector('.tui-page-btn');
 
 const gall = document.querySelector('.gallery__list');
 
@@ -12,24 +13,27 @@ const filmsPromise = unsplashApi.fetchPopularFilms();
 const filmGenres = unsplashApi.findGenre();
 
 filmsPromise.then(result => {
-  const films = result.data.results;
+  const films = result.data;
   renderFilms(films);
   // console.log(films[0].genre_ids);
 });
 
 function renderFilms(films) {
-  
-  filmGenres.then(result => {
-    const genre = result.data.genres;
-    const genName = genre.map(genre => genre.name);
-    const genId = genre.map(genre => genre.id);
-    console.log(genName);
-    console.log(genId);
-    localStorage.setItem(genId, genName);
-  });
-  
-  
-  const markup = films
+  // filmGenres.then(result => {
+  //   const genre = result.data.genres;
+  //   const genName = genre.map(genre => genre);
+  //   // const genId = genre.map(genre => genre.id);
+  //   console.log(genName);
+  //   // console.log(genId);
+  //   localStorage.setItem('genId', genName);
+  // });
+
+  console.log(films);
+  createPagination(films);
+
+  // onPageBtnClick
+
+  const markup = films.results
     .map(film => {
       return `
       <li class="gallery__item">
@@ -45,3 +49,15 @@ function renderFilms(films) {
     .join('');
   return (gall.innerHTML = markup);
 }
+
+// const onPageBtnClick = async event => {
+//   try {
+//    if(paginationBtn.textContent === )
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// paginationBtn.addEventListener('click', onPageBtnClick);
+
+// console.log(paginationBtn);
