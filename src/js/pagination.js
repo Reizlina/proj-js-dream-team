a3ca1e683f54db9b641819c7fbd25519a155d45e
IgorRyabenko <riabenko.igor@gmail.com>
2022-06-13 21:00:59 +0300

    commit

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -5,33 +5,51 @@ const unsplashApi = new UnsplashApi();
 const containerPagination = document.querySelector('#pagination');
 // console.log(unsplashApi.fetchPopularFilms());
 
-export const createPagination = ({ total_results, total_pages } = {}) => {
+export const createPagination = ({ totalItems, totalPages, page } = {}) => {
   const options = {
-    totalItems: total_results,
-    itemsPerPage: total_results / total_pages,
+    totalItems,
+    itemsPerPage: totalItems / totalPages,
     visiblePages: 5,
-    page: 1,
+    page,
     centerAlign: true,
     firstItemClassName: 'tui-first-child',
     lastItemClassName: 'tui-last-child',
+    // template: {
+    //   page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    //   currentPage:
+    //     '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    //   moveButton:
+    //     '<a href="#" class="tui-page-btn tui-{{type}}">' +
+    //     '<span class="tui-ico-{{type}}">{{type}}</span>' +
+    //     '</a>',
+    //   disabledMoveButton:
+    //     '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+    //     '<span class="tui-ico-{{type}}">{{type}}</span>' +
+    //     '</span>',
+    //   moreButton:
+    //     '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+    //     '<span class="tui-ico-ellip">...</span>' +
+    //     '</a>',
+    // },
     template: {
       page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-      currentPage:
-        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
-      disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span class="tui-ico-ellip">...</span>' +
-        '</a>',
-    },
-  };
+      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      disabledMoveButton(event) {
+        if (event.type === 'first' || event.type === 'last') {
+          return '<span></span>';
+        }
+
+        return `<span class="tui-page-btn tui-is-disabled tui-${event.type}"><span class="tui-ico-${event.type}">${event.type}</span></span>`;
+      },
+      moveButton(event) {
+        if (event.type === 'first' || event.type === 'last') {
+          return '<span></span>';
+        }
+
+        return `<a href="#" class="tui-page-btn tui-${event.type}"><span class="tui-ico-${event.type}">${event.type}</span></a>`;
+      },
+  }
+}
   const pagination = new Pagination(containerPagination, options);
   // paganation.on('afterMove', event => {
   //   const currentPage = event.page;
